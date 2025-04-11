2a2dec41746d6972aff784e6dec76b8c8099071d
Everton Goncalves <everton.goncalves.89@gmail.com>
2025-04-11 12:29:06 -0300

    feat: command cli

--- src/main/java/com/egon89/stresstest/HttpStress.java
+++ src/main/java/com/egon89/stresstest/HttpStress.java
@@ -7,38 +7,42 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.*;
 
 public class HttpStress {
-  public void execute(String url, String method, int totalRequest, int concurrentRequest, int intervalSec, String body, Map<String, String> headers) throws InterruptedException, ExecutionException {
+  private static final List<String> VALID_METHODS = List.of("GET", "HEAD", "PATCH", "POST", "PUT", "DELETE");
+
+  public void execute(HttpStressInput input) throws InterruptedException, ExecutionException {
     System.out.println("Starting stress test...");
     System.out.printf("URL: %s, Method: %s, Request(s): %d, Concurrency: %d%n",
-        url, method, totalRequest, concurrentRequest);
+        input.url, input.method, input.totalRequest, input.concurrentRequest);
 
     var startTime = Instant.now();
     try (var client = HttpClient.newHttpClient()) {
       var resultQueue = new LinkedBlockingQueue<Result>();
-      var semaphore = new ArrayBlockingQueue<Object>(concurrentRequest);
+      var semaphore = new ArrayBlockingQueue<Object>(input.concurrentRequest);
 
       try(var scope = new StructuredTaskScope.ShutdownOnFailure()) {
         scope.fork(() -> {
-          processResult(resultQueue, totalRequest);
+          processResult(resultQueue, input.totalRequest);
           return null;
         });
 
-        for (int i = 0; i < totalRequest; i++) {
+        for (int i = 0; i < input.totalRequest; i++) {
           final var requestNumber = i + 1;
           semaphore.put(new Object());
 
           scope.fork(() -> {
             var requestStart = Instant.now();
             try {
-              var bodyRequest = body.isEmpty()
-                  ? HttpRequest.BodyPublishers.noBody() : HttpRequest.BodyPublishers.ofString(body);
-              var requestBuilder = HttpRequest.newBuilder(URI.create(url))
-                  .method(method, bodyRequest);
-              headers.forEach(requestBuilder::header);
+              var bodyRequest = input.body.isEmpty()
+                  ? HttpRequest.BodyPublishers.noBody() : HttpRequest.BodyPublishers.ofString(input.body);
+              var requestBuilder = HttpRequest.newBuilder(URI.create(input.url))
+                  .method(input.method, bodyRequest);
+              input.headers.forEach(requestBuilder::header);
               var request = requestBuilder.build();
 
               var response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -50,11 +54,12 @@ public class HttpStress {
               System.out.printf("Request %d: Status Code: %d (%dms)%n",
                   requestNumber, result.statusCode(), result.duration().toMillis());
             } catch (Exception e) {
+              System.err.println(e);
               resultQueue.put(new Result(-1, Duration.ZERO));
             }
 
-            if (intervalSec > 0) {
-              Thread.sleep(intervalSec * 1000L);
+            if (input.intervalSec > 0) {
+              Thread.sleep(input.intervalSec * 1000L);
             }
             semaphore.take();
 
@@ -68,6 +73,10 @@ public class HttpStress {
 
     var totalDuration = Duration.between(startTime, Instant.now());
     System.out.printf("Total Duration: %s(ms)%n", totalDuration.toMillis());
+  }
+
+  public static void validate(HttpStressInput input) {
+
   }
 
   private void processResult(BlockingQueue<Result> resultQueue, int totalRequest) throws InterruptedException {
@@ -90,6 +99,43 @@ public class HttpStress {
         System.out.printf("Status: %d, Counter:%d%n", statusCode, counter));
     System.out.printf("Average response time: %d(ms)%n", averageResponseTime);
 
+  }
+
+  public record HttpStressInput(
+      String url,
+      String method,
+      int totalRequest,
+      int concurrentRequest,
+      int intervalSec,
+      String body,
+      Map<String, String> headers
+  ) {
+    public HttpStressInput {
+      if (Objects.isNull(url) || url.isBlank()) {
+        throw new IllegalArgumentException("url must not be null or blank");
+      }
+
+      if (Objects.isNull(method) || method.isBlank()) {
+        throw new IllegalArgumentException("method must not be null or blank");
+      }
+
+      if (totalRequest <= 0) {
+        throw new IllegalArgumentException("totalRequest must be positive");
+      }
+
+      if (concurrentRequest <= 0) {
+        throw new IllegalArgumentException("concurrentRequest must be positive");
+      }
+
+      if (intervalSec < 0) {
+        throw new IllegalArgumentException("intervalSec must be zero or positive");
+      }
+
+      if(!VALID_METHODS.contains(method.toUpperCase())) {
+        throw new IllegalArgumentException(
+            "invalid HTTP method: %s. Allowed methods are: GET, HEAD, PATCH, POST, PUT, DELETE".formatted(method));
+      }
+    }
   }
 
   private record Result(int statusCode, Duration duration) {}
--- src/main/java/com/egon89/stresstest/StressTestApplication.java
+++ src/main/java/com/egon89/stresstest/StressTestApplication.java
@@ -1,21 +1,78 @@
 package com.egon89.stresstest;
 
+import picocli.CommandLine;
+
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
-public class StressTestApplication {
+@CommandLine.Command(
+    name = "rate-limiter",
+    mixinStandardHelpOptions = true,
+    version = "rate-limiter 1.0",
+    description = "Rate limiter")
+public class StressTestApplication implements Runnable {
+
+  @CommandLine.Option(names = {"-u", "--url"}, description = "The target URL for the stress test", required = true)
+  private String url;
+
+  @CommandLine.Option(names = {"-r", "--requests"}, description = "The total number of requests to send")
+  private Integer totalRequest = 10;
+
+  @CommandLine.Option(names = {"-c", "--concurrency"}, description = "The number of concurrent requests")
+  private Integer concurrentRequest = 2;
+
+  @CommandLine.Option(names = {"-X", "--method"}, description = "The HTTP method (e.g., GET, HEAD, PATCH, POST, PUT)")
+  private String method = "GET";
+
+  @CommandLine.Option(names = {"-i", "--interval"}, description = "The interval (in seconds) between requests. Default is 0")
+  private Integer intervalSec = 0;
+
+  @CommandLine.Option(names = {"-d", "--body"}, description = "The request body for requests")
+  private String body = "";
+
+  @CommandLine.Option(names = {"-H", "--headers"}, description = "Custom headers for the request separated by commas")
+  private String headersStr;
+
   public static void main(String[] args) {
-    var url = "http://example.com";
-    var method = "GET";
-    var totalRequest = 100;
-    var concurrentRequest = 2;
-    var intervalSec = 0;
-    var body = "";
-    var headers = Map.of("User-Agent", "java-stress-test/1.0");
+    int code = new CommandLine(new StressTestApplication()).execute(args);
+    System.exit(code);
+  }
 
+  @Override
+  public void run() {
+    var input = new HttpStress.HttpStressInput(
+        url,
+        method,
+        totalRequest,
+        concurrentRequest,
+        intervalSec,
+        body,
+        parseHeaders(headersStr)
+    );
     try {
-      new HttpStress().execute(url, method, totalRequest, concurrentRequest, intervalSec, body, headers);
+      new HttpStress().execute(input);
     } catch (Exception e) {
-      System.err.println("something goes wrong!");
+      throw new RuntimeException(e);
     }
+  }
+
+  private static Map<String, String> parseHeaders(String headersStr) {
+    if (Objects.isNull(headersStr) || headersStr.isBlank()) {
+      return Collections.emptyMap();
+    }
+
+    return Arrays.stream(headersStr.split(","))
+        .map(String::trim)
+        .filter(pair -> pair.contains("="))
+        .map(pair -> pair.split("=", 2)) // limit=2 to avoid extra '=' in values
+        .filter(kv -> kv.length == 2 && !kv[0].isBlank())
+        .collect(Collectors.toMap(
+            kv -> kv[0].trim(),
+            kv -> kv[1].trim(),
+            (v1, v2) -> v2 // if a duplicate key is found, keep the second value (v2) and discard the first (v1).
+        ));
   }
 }
